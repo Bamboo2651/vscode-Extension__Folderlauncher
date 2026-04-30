645a61d5e3ba18215ec039cdbf5d58a97e8af458
tkhy2651 <tah331925@gmail.com>
2026-04-30 16:15:57 +0900

    t-07

---
+++ folder-launcher/src/FolderItem.ts
@@ -0,0 +1,16 @@
+import * as vscode from 'vscode';
+
+export class FolderItem extends vscode.TreeItem{
+    constructor(
+        label: string,
+        public readonly folderPath: string,
+        public readonly itemType: 'root' | 'folder'
+    ) {
+        super(label);
+        if(itemType === 'root') {
+            this.iconPath = new vscode.ThemeIcon('root-folder');
+        } else {
+            this.iconPath = new vscode.ThemeIcon('folder');
+        }
+    }
+}

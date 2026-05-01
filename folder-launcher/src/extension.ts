6a2c3761d4ce84107f733791b01d2f343ff39d87
tkhy2651 <tah331925@gmail.com>
2026-05-01 21:00:28 +0900

    up

--- folder-launcher/src/extension.ts
+++ folder-launcher/src/extension.ts
@@ -1,7 +1,17 @@
 import * as vscode from 'vscode';
+import { RootManager } from './RootManager';
+import { FolderProvider } from './FolderProvider';
 
 export function activate(context: vscode.ExtensionContext) {
-    console.log('FolderLauncher activated');
+    // console.log('FolderLauncher activated');
+    const rootManager = new RootManager(context.globalState);
+    const folderProvider = new FolderProvider(rootManager)
+
+    const treeView = vscode.window.createTreeView('folderLauncher', {
+        treeDataProvider: folderProvider,
+        showCollapseAll: true
+    });
+    context.subscriptions.push(treeView);
 }
 
 export function deactivate() {}

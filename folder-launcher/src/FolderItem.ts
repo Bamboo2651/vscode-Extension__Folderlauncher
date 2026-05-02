498dc3b4a49e33108d772f7f81413be9715177e9
tkhy2651 <tah331925@gmail.com>
2026-05-02 13:08:09 +0900

    up

--- folder-launcher/src/FolderItem.ts
+++ folder-launcher/src/FolderItem.ts
@@ -1,16 +1,23 @@
 import * as vscode from 'vscode';
 
-export class FolderItem extends vscode.TreeItem{
+export class FolderItem extends vscode.TreeItem {
     constructor(
         label: string,
         public readonly folderPath: string,
         public readonly itemType: 'root' | 'folder'
     ) {
         super(label);
-        if(itemType === 'root') {
+
+        if (itemType === 'root') {
             this.iconPath = new vscode.ThemeIcon('root-folder');
+            this.collapsibleState = vscode.TreeItemCollapsibleState.Collapsed;
         } else {
             this.iconPath = new vscode.ThemeIcon('folder');
+            this.command = {
+                command: 'folderLauncher.openFolder',
+                title: 'Open Folder',
+                arguments: [this], 
+            };
         }
     }
 }
